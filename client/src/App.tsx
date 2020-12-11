7b55fa5d24f045538e0bc1ba178c194c629240c9
kev-damian <maheu@outlook.com>
2020-12-11 12:58:09 +0900

    Header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.scss";
 import Overview from "./components/Overview";
+import {
+  Header,
+  HeaderName
+} from "carbon-components-react/lib/components/UIShell";
 
 function App() {
   return (
     <div className="App" style={{ height: "100%" }}>
+      <Header aria-label="CHIpotle">
+      <HeaderName href="#" prefix="">
+        CHIPotle
+      </HeaderName>
+    </Header>
       <Overview />
     </div>
   );
