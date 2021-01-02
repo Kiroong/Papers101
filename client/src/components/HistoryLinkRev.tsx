2a9fb0e7fb2b7e5dc4ceb1577f9b1cbef4dd5462
SEOKWEON JUNG <minimus@snu.ac.kr>
2021-01-03 02:40:57 +0900

    history view merged into single svg

--- client/src/components/HistoryLinkRev.tsx
+++ client/src/components/HistoryLinkRev.tsx
@@ -35,41 +35,169 @@ const HistoryLink: React.FC<Props> = ({
     setHoveredEntry,
 }) => {
     const svgHeight: number = cellHeight * 50
-    const [prevEntries, setPrevEntries] = useState<PaperEntry[]>([])
+    const [prevHistoryLength, setPrevHistoryLength] = useState<number>(0)
     const root = useRef<HTMLDivElement>(null)
 
-    let isEntriesEqual = (newEntries: PaperEntry[]): boolean => {
-        let prevDois: string[] = prevEntries.map((d) => d.doi)
-        let nextDois: string[] = newEntries.map((d) => d.doi)
-        if (prevEntries.length === 0) {
-            return true
-        }
-        if (prevDois.length != nextDois.length) {
-            return true
-        }
+    useEffect(() => {
+        // Init
 
-        for (let i in prevDois) {
-            if (prevDois[i] !== nextDois[i]) {
-                return true
+        let _root = d3
+            .select(root.current)
+            .select('svg')
+            .select('.history-content')
+    }, [])
+
+    /*
+        If the length of the histories change,
+        then move every existing elements to the left
+        and add new group at the end of the svg.
+    */
+    useEffect(() => {
+        if (histories.length !== prevHistoryLength) {
+            console.log(histories)
+            console.log(numHistories)
+            // update
+            const _root = d3
+                .select(root.current)
+                .select('svg')
+                .select('.history-content')
+            // Update previous history length
+            setPrevHistoryLength(histories.length)
+
+            if (histories.length === 1) {
+                const _lineData: HistoryLine[] = []
+                const _newHistory = histories[histories.length - 1].slice(0, 50)
+                const topKDois: string[] = _newHistory
+                    .slice(0, 10)
+                    .map((d) => d.doi);
+
+                    const newHistory = _root.append('g').classed('hg0', true)
+                    newHistory
+                    .selectAll('.node')
+                    .data(_lineData)
+                    .join('circle')
+                    .classed('node', true)
+                    .attr('cx', svgWidth)
+                    .attr('cy', (d: HistoryLine, i: number) => {
+                        return (i + 0.5) * cellHeight
+                    })
+                    .attr('r', 3)
+                    .attr('opacity', (d: HistoryLine) => {
+                        if (topKDois.includes(d.fromDoi)) {
+                            return 0.7
+                        } else {
+                            return 0.1
+                        }
+                    })
+
+            } else if (histories.length > 1) {
+                // move existing elements to the left
+                for (let n = numHistories - 1; n >= 0; n--) {
+                    console.log('hg' + n)
+                    _root
+                        .selectAll('.hg' + n)
+                        .classed('hg' + n, false)
+                        .classed('hg' + (n + 1), true)
+                }
+
+                // add new elment
+                const _lineData: HistoryLine[] = []
+                const _newHistory = histories[histories.length - 1].slice(0, 50)
+                const _prevHistory = histories[histories.length - 2].slice(
+                    0,
+                    50
+                )
+                const topKDois: string[] = _newHistory
+                    .slice(0, 10)
+                    .map((d) => d.doi)
+                _newHistory.forEach((te, ti) => {
+                    let fi = _prevHistory.findIndex((fe) => fe.doi === te.doi)
+                    _lineData.push({
+                        fromIndex: fi,
+                        fromDoi: fi >= 0 ? _prevHistory[fi].doi : '',
+                        toIndex: ti,
+                        toDoi: te.doi,
+                    })
+                })
+                const newHistory = _root.append('g').classed('hg0', true)
+
+                newHistory
+                    .selectAll('.parallel')
+                    .data(_lineData)
+                    .join('line')
+                    .classed('history', true)
+                    .classed('parallel', true)
+                    .attr(
+                        'x1',
+                        (d: HistoryLine) =>
+                            d.fromIndex >= 0 ? svgWidth : svgWidth
+                        //d.fromIndex >= 0 ? 0 : svgWidth * (0.95)
+                    )
+                    .attr('y1', (d: HistoryLine) =>
+                        d.fromIndex >= 0
+                            ? (d.fromIndex + 0.5) * cellHeight
+                            : (d.toIndex + 0.5) * cellHeight
+                    )
+                    .attr('x2', svgWidth + svgWidth / 5)
+                    .attr(
+                        'y2',
+                        (d: HistoryLine) => (d.toIndex + 0.5) * cellHeight
+                    )
+                    .attr('stroke', (d) => {
+                        if (topKDois.includes(d.toDoi)) {
+                            return d3.schemeSet1[3]
+                        } else {
+                            return d3.schemeSet1[8]
+                        }
+                    })
+                    .attr('stroke-width', 3) //cellHeight * 0.6)
+                    .attr('stroke-linecap', 'round')
+                    .attr('opacity', (d) => {
+                        if (topKDois.includes(d.toDoi)) {
+                            return 0.7
+                        } else {
+                            return 0.1
+                        }
+                    })
+
+                newHistory
+                    .selectAll('.node')
+                    .data(_lineData)
+                    .join('circle')
+                    .classed('node', true)
+                    .attr('cx', svgWidth)
+                    .attr('cy', (d: HistoryLine, i: number) => {
+                        return (i + 0.5) * cellHeight
+                    })
+                    .attr('r', 3)
+                    .attr('opacity', (d: HistoryLine) => {
+                        if (topKDois.includes(d.fromDoi)) {
+                            return 0.7
+                        } else {
+                            return 0.1
+                        }
+                    })
+
+                for (let n = 0; n <= numHistories; n++) {
+                    _root
+                        .selectAll('.hg' + n)
+                        .transition()
+                        .duration(1000)
+                        .attr(
+                            'transform',
+                            translate((-svgWidth / 5) * (n + 1), 0)
+                        )
+                }
+
+                _root
+                    .selectAll('.hg5')
+                    .remove()
             }
         }
-        return false
-    }
-
+    }, [histories])
 
     useEffect(() => {
-        console.log('num change')
-        if(numHistories >= 5) {
-
-        }
-        else {
-
-        }
-    }, [numHistories])
-
-    useEffect(() => {
-        console.log('hover')
-
+        //console.log('hover')
     }, [hoveredEntry])
 
     return (
@@ -78,21 +206,25 @@ const HistoryLink: React.FC<Props> = ({
                 className={'history-header'}
                 style={{ width: svgWidth, height: offsetHeight }}
             >
-            
                 {[...Array(numHistories)].map((n, i) => {
-                    return(
-                        <div style={{ width: svgWidth/numHistories, height: offsetHeight, float:'left'}}>
+                    return (
+                        <div
+                            style={{
+                                width: svgWidth / numHistories,
+                                height: offsetHeight,
+                                display: 'inline-flex',
+                            }}
+                        >
                             버튼공간
                         </div>
                     )
-                })
-                }
+                })}
             </div>
             <svg height={svgHeight} width={svgWidth}>
                 <g
                     className={'history-content'}
                     height={svgHeight}
-                    width={svgWidth}    
+                    width={svgWidth}
                 ></g>
             </svg>
         </div>
